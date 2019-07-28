587d5d5e768271af6e9ec8c4b3b0f2fe8beaa1e5
David Blackman <david@blackmad.com>
2019-07-27 23:01:13 -0400

    public ip

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-REDIS_URL=redis://localhost:6379  gunicorn --worker-class eventlet -w 1 --timeout 300 chat:app
+REDIS_URL=redis://localhost:6379  gunicorn --worker-class eventlet -w 1 --timeout 300 chat:app -b 0.0.0.0:8000
 
