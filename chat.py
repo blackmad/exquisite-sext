f8f6c0ce445d85d6fde5837c906f0c0f3286348e
David Blackman <david@blackmad.com>
2019-09-20 09:34:44 -0400

    updated css for mobile

--- chat.py
+++ chat.py
@@ -1,7 +1,8 @@
 #!/bin/env python
 from app import create_app, socketio
 
-app = create_app(debug=False)
+app = create_app(debug=True)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0')
